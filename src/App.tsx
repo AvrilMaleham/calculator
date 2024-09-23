f991d2ce9a88288ad268edfa5b84100a55a7283c
Avril Maleham <avrilmaleham@outlook.com>
2024-09-23 13:32:46 +1200

    feat: remove unused boilerplate and adapt to basic calc needs

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [value, setValue] = useState("");
 
   return (
     <>
+      <div>{value}</div>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => setValue((value) => value + "1")}>1</button>
+        <button onClick={() => setValue((value) => value + "2")}>2</button>
+        <button onClick={() => setValue((value) => value + "3")}>3</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        <button onClick={() => setValue((value) => value + "4")}>4</button>
+        <button onClick={() => setValue((value) => value + "5")}>5</button>
+        <button onClick={() => setValue((value) => value + "6")}>6</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <button onClick={() => setValue((value) => value + "7")}>7</button>
+        <button onClick={() => setValue((value) => value + "8")}>8</button>
+        <button onClick={() => setValue((value) => value + "9")}>9</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
