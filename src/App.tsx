de9b097f623c7426d50a8d02de44c180b9750ed0
Avril Maleham <avrilmaleham@outlook.com>
2024-09-23 13:40:58 +1200

    feat: flesh out buttons

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,25 @@ function App() {
         <button onClick={() => setValue((value) => value + "1")}>1</button>
         <button onClick={() => setValue((value) => value + "2")}>2</button>
         <button onClick={() => setValue((value) => value + "3")}>3</button>
+        <button onClick={() => setValue((value) => value + "/")}>/</button>
       </div>
       <div>
         <button onClick={() => setValue((value) => value + "4")}>4</button>
         <button onClick={() => setValue((value) => value + "5")}>5</button>
         <button onClick={() => setValue((value) => value + "6")}>6</button>
+        <button onClick={() => setValue((value) => value + "x")}>x</button>
       </div>
       <div>
         <button onClick={() => setValue((value) => value + "7")}>7</button>
         <button onClick={() => setValue((value) => value + "8")}>8</button>
         <button onClick={() => setValue((value) => value + "9")}>9</button>
+        <button onClick={() => setValue((value) => value + "-")}>-</button>
+      </div>
+      <div>
+        <button onClick={() => setValue((value) => value + "0")}>0</button>
+        <button onClick={() => setValue("")}>C</button>
+        <button onClick={() => setValue((value) => value + "=")}>=</button>
+        <button onClick={() => setValue((value) => value + "+")}>+</button>
       </div>
     </>
   );
